0aecd0e65fdf73ca440b0489ea0442b36b8e1e75
Jorge Robles <jorgejoanrobles@gmail.com>
2024-03-01 12:27:29 +0100

    mise a jour navbar

--- partials/header.php
+++ partials/header.php
@@ -14,35 +14,42 @@
 
 <!-- Navbar START --> 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand fs-4" href="#">ComparOperateur</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+  <div class="container">
+    <!-- Logo --> 
+    <a class="navbar-brand fs-4" href="#"> <img src="../images/FlightFinder-removebg-preview.png" width="80px" class="px-2"alt="">Flight-Finder</a>
+    <!-- Toggle Btn --> 
+    <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
+
+    <!--  SideBar -->
+    <div class="sidebar offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+        <!--  SideBar Header -->
+
+      <div class="offcanvas-header text-white border-bottom">
         <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
+
+        <!--  SideBar Body -->
+      <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
+        <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+          <li class="nav-item mx-2">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#Link">Link</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#Services">Services</a>
+          </li>
             </ul>
-          </li>
-        </ul>
+        <!--  Login In - Sign Up -->
+        <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
+            <a href="#Login" class="text-white">Login</a>
+            <a href="#SignUp" class="ButtonSignUp text-black text-decoration-none px-3 py-1 rounded-4">Sign Up</a>
+        </div>
+
       </div>
     </div>
   </div>
